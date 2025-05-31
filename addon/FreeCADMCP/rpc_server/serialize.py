8243f2c3a01c4a0d96c61754f0fcdc66533e2594
Ralph Apel <r.apel@r-apel.de>
2025-05-31 08:54:23 -0400

    expand serialize

--- addon/FreeCADMCP/rpc_server/serialize.py
+++ addon/FreeCADMCP/rpc_server/serialize.py
@@ -1,10 +1,79 @@
 import FreeCAD as App
+import Part
+import Materials
 import json
+import sys
 
+sys.setrecursionlimit(8000)
 
 def serialize_value(value):
-    if isinstance(value, (int, float, str, bool)):
+    if isinstance(value, (int, float, str, bool, dict)):
         return value
+    elif isinstance(value, Part.Face):
+        return {
+            "Area": serialize_value(value.Area),
+            "BoundBox": serialize_value(value.BoundBox),
+            "CenterOfGravity": serialize_value(value.CenterOfGravity),
+            "CenterOfMass": serialize_value(value.CenterOfMass),
+            "CompSolids": serialize_value(value.CompSolids),
+            "Compounds": serialize_value(value.Compounds),
+            "Content": serialize_value(value.Content),
+            "Edges": serialize_value(value.Edges),
+            "ElementMap": serialize_value(value.ElementMap),
+            "ElementMapSize": serialize_value(value.ElementMapSize),
+            "ElementMapVersion": serialize_value(value.ElementMapVersion),
+            "ElementReverseMap": serialize_value(value.ElementReverseMap),
+            #"Faces": serialize_value(value.Faces),
+            "Hasher": serialize_value(value.Hasher),
+            "Length": serialize_value(value.Length),
+            "Mass": serialize_value(value.Mass),
+            "MatrixOfInertia": serialize_value(value.MatrixOfInertia),
+            "MemSize": serialize_value(value.MemSize),
+            "Module": serialize_value(value.Module),
+            "Orientation": serialize_value(value.Orientation),
+            "OuterWire": serialize_value(value.OuterWire),
+            "ParameterRange": serialize_value(value.ParameterRange),
+            "Placement": serialize_value(value.Placement),
+            "PrincipalProperties": serialize_value(value.PrincipalProperties),
+            "ShapeType": serialize_value(value.ShapeType),
+            "Shells": serialize_value(value.Shells),
+            "Solids": serialize_value(value.Solids),
+            "StaticMoments": serialize_value(value.StaticMoments),
+            "SubShapes": serialize_value(value.SubShapes),
+            "Surface": serialize_value(value.Surface),
+            "Tag": serialize_value(value.Tag),
+            "Tolerance": serialize_value(value.Tolerance),
+            "TypeId": serialize_value(value.TypeId),
+            "Vertexes": serialize_value(value.Vertexes),
+            "Volume": serialize_value(value.Volume),
+            "Wire": serialize_value(value.Wire),
+            "Wires": serialize_value(value.Wires)          
+        }
+    elif isinstance(value, Materials.Material):
+        return {
+            "AppearanceModels": serialize_value(value.AppearanceModels),
+            "AppearanceProperties": serialize_value(value.AppearanceProperties),
+            "Author": serialize_value(value.Author),
+            "AuthorAndLicense": serialize_value(value.AuthorAndLicense),
+            "Description": serialize_value(value.Description),
+            "Directory": serialize_value(value.Directory),
+            "LegacyProperties": serialize_value(value.LegacyProperties),
+            "LibraryIcon": serialize_value(value.LibraryIcon),
+            "LibraryName": serialize_value(value.LibraryName),
+            "LibraryRoot": serialize_value(value.LibraryRoot),
+            "License": serialize_value(value.License),
+            "Module": serialize_value(value.Module),
+            "Name": serialize_value(value.Name),
+            "Parent": serialize_value(value.Parent),
+            "PhysicalModels": serialize_value(value.PhysicalModels),
+            "PhysicalProperties": serialize_value(value.PhysicalProperties),
+            "Properties": serialize_value(value.Properties),
+            "Reference": serialize_value(value.Reference),
+            "Tags": serialize_value(value.Tags),
+            "TypeId": serialize_value(value.TypeId),
+            "URL": serialize_value(value.URL),
+            "UUID": serialize_value(value.UUID)
+        }
     elif isinstance(value, App.Vector):
         return {"x": value.x, "y": value.y, "z": value.z}
     elif isinstance(value, App.Rotation):
@@ -19,10 +88,10 @@ def serialize_value(value):
         }
     elif isinstance(value, (list, tuple)):
         return [serialize_value(v) for v in value]
-    elif isinstance(value, (App.Color)):
-        return tuple(value)
+    #elif isinstance(value, (App.Color)):
+    #    return tuple(value)
     else:
-        return str(value)
+        return str(value.__class__)
 
 
 def serialize_shape(shape):
